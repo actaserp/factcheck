21f2c04eb11e015661bd1c78a4fa144c33033bff
LeeJaeWook <ljw960214@naver.com>
2025-01-14 18:03:36 +0900

    250114_이재욱_대시보드 작업중

---
+++ src/main/java/mes/app/dashboard/FactCheckDashBoardController.java
@@ -0,0 +1,144 @@
+package mes.app.dashboard;
+
+import lombok.extern.slf4j.Slf4j;
+import mes.app.dashboard.service.FactCheckDashBoardService;
+import mes.app.system.SystemLogController;
+import mes.domain.model.AjaxResult;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.text.SimpleDateFormat;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/factDashboard")
+public class FactCheckDashBoardController {
+
+    @Autowired
+    private FactCheckDashBoardService factService;
+    @Autowired
+    private SystemLogController systemLogController;
+
+    // 금일 신규 사용자
+    @GetMapping("/todaySignup")
+    private AjaxResult todaySignup() {
+        AjaxResult result = new AjaxResult();
+        try{
+            List<Map<String, Object>> SignupList = factService.todaySignup();
+            for(Map<String, Object> item : SignupList) {
+                item.put("no", SignupList.indexOf(item) + 1);
+                String indatem = new SimpleDateFormat("yyyy-MM-dd").format(item.get("INDATEM"));
+                item.remove("INDATEM");
+                item.put("INDATEM", indatem);
+            }
+            result.data = SignupList;
+            log.info("SignupList : {}", SignupList);
+
+            result.message = "신규 사용자 조회에 성공했습니다.";
+        }catch (Exception e){
+            e.printStackTrace();
+            result.message = "신규 사용자 조회에 실패했습니다.";
+        }
+        return result;
+    }
+    // 금일 상위 검색지역
+    @GetMapping("/todayAddress")
+    private AjaxResult todayAddress() {
+        AjaxResult result = new AjaxResult();
+        try{
+            List<Map<String, Object>> AddressList = factService.todayAddress();
+            for(Map<String, Object> item : AddressList) {
+                item.put("no", AddressList.indexOf(item) + 1);
+                String indatem = new SimpleDateFormat("yyyy-MM-dd").format(item.get("REQDATE"));
+                item.remove("REQDATE");
+                item.put("REQDATE", indatem);
+            }
+            result.data = AddressList;
+            result.message = "검색지역 조회에 성공했습니다.";
+        }catch (Exception e){
+            e.printStackTrace();
+            result.message = "검색지역 조회에 실패했습니다.";
+        }
+        return result;
+    }
+    // 1:1 미답변 문의글
+    @GetMapping("/QnAList")
+    private AjaxResult QnAList() {
+        AjaxResult result = new AjaxResult();
+        try{
+            List<Map<String, Object>> QnAList = factService.QnAList();
+            for(Map<String, Object> item : QnAList) {
+                item.put("no", QnAList.indexOf(item) + 1);
+                // 날자 부분만 추출
+                String indatem = new SimpleDateFormat("yyyy-MM-dd").format(item.get("INDATEM"));
+                // 시간 부분만 추출
+                String time = new SimpleDateFormat("HH시 mm분 ss초").format(item.get("INDATEM"));
+                item.remove("INDATEM");
+                item.put("INDATEM", indatem);
+                item.put("INDATEMTIME", time);
+            }
+            result.data = QnAList;
+            result.message = "미답변 문의글 조회에 성공했습니다.";
+        }catch (Exception e){
+            e.printStackTrace();
+            result.message = "미답변 문의글 조회에 실패했습니다.";
+        }
+        return result;
+    }
+    // 차트데이터
+    @GetMapping("/createCharts")
+    private AjaxResult createCharts() {
+        AjaxResult result = new AjaxResult();
+        try{
+            // 일주일 가입자현황
+            List<Map<String, Object>> SignupOfWeek = factService.SignupOfWeek();
+            // 유출자현황
+            List<Map<String, Object>> Signout = factService.Signout();
+            // 등기열람건수
+            List<Map<String, Object>> CntOfRegister = factService.CntOfRegister();
+            // 상위검색지역
+            List<Map<String, Object>> BestSearch = factService.BestSearch();
+
+            Map<String, Object> items = new HashMap<String, Object>();
+            items.put("SignupOfWeek", SignupOfWeek);
+            items.put("Signout", Signout);
+            items.put("CntOfRegister", CntOfRegister);
+            items.put("BestSearch", BestSearch);
+            result.data = items;
+            result.message = "차트데이터 조회에 성공했습니다.";
+        }catch (Exception e){
+            e.printStackTrace();
+            result.message = "차트데이터 조회에 실패했습니다.";
+        }
+        return result;
+    }
+    // 카드 데이터
+    @GetMapping("/cardDatas")
+    private AjaxResult cardDatas() {
+        AjaxResult result = new AjaxResult();
+        try{
+            List<Map<String, Object>> CardList = factService.cardDatas();
+            // 금일 신규사용자 집계
+
+            // 금일 상위 검색지역 집계
+
+            // 총 가입자수 집계
+
+            // 금일 등기 열람건 수 집계
+
+            // 미답변 문의건 수 집계
+
+            result.data = CardList;
+            result.message = "신규 사용자 조회에 성공했습니다.";
+        }catch (Exception e){
+            e.printStackTrace();
+            result.message = "신규 사용자 조회에 실패했습니다.";
+        }
+        return result;
+    }
+}
---
+++ src/main/java/mes/app/dashboard/service/FactCheckDashBoardService.java
@@ -0,0 +1,198 @@
+package mes.app.dashboard.service;
+
+import mes.domain.services.SqlRunner;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class FactCheckDashBoardService {
+
+    @Autowired
+    SqlRunner sqlRunner;
+    // 금일 신규 사용자
+    public List<Map<String, Object>> todaySignup() {
+
+        String sql = """
+            SELECT
+                INDATEM,
+                USERNM,
+                USERADDR,
+                AGENUM,
+                USERHP
+            FROM
+                TB_USERINFO
+            WHERE
+                CONVERT(DATE, INDATEM) = CONVERT(DATE, GETDATE());
+            """;
+
+        List<Map<String,Object>> items = this.sqlRunner.getRows(sql, null);
+
+        return items;
+    }
+    // 금일 상위 검색지역
+    public List<Map<String, Object>> todayAddress() {
+
+        String sql = """
+                SELECT
+                    r.REALID,
+                    r.USERID,
+                    s.REQDATE,
+                    r.REALADD,
+                    r.REGDATE,
+                    r.RESIDO,
+                    r.REGUGUN,
+                    r.REMOK,
+                    r.REWON,
+                    r.REWONDATE,
+                    r.REJIMOK,
+                    r.REAREA,
+                    r.REAMOUNT,
+                    r.RESEQ,
+                    r.REMAXAMT,
+                    r.INDATEM,
+                    r.REALSCORE,
+                    r.REALPOINT,
+                    r.RELASTDATE,
+                    COUNT(s.REALID) AS VIEW_COUNT
+                FROM
+                    TB_REALINFO r
+                LEFT JOIN
+                    TB_SEARCHINFO s ON r.REALID = s.REALID
+                WHERE
+                    CAST(s.REQDATE AS DATE) = CAST(GETDATE() AS DATE)
+                GROUP BY
+                    r.REALID, r.USERID, s.REQDATE, r.REALADD, r.REGDATE,
+                    r.RESIDO, r.REGUGUN, r.REMOK, r.REWON, r.REWONDATE,
+                    r.REJIMOK, r.REAREA, r.REAMOUNT, r.RESEQ, r.REMAXAMT,
+                    r.INDATEM, r.REALSCORE, r.REALPOINT, r.RELASTDATE
+                ORDER BY
+                    VIEW_COUNT DESC;
+            """;
+
+        List<Map<String,Object>> items = this.sqlRunner.getRows(sql, null);
+
+        return items;
+    }
+    // 1:1 미답변 문의글
+    public List<Map<String, Object>> QnAList() {
+
+        String sql = """
+            SELECT
+                Q1.QSTSEQ,
+                Q1.USERID,
+                Q1.QSTTEXT,
+                Q1.QSTTEL,
+                Q1.INDATEM
+            FROM
+                TB_USERQST Q1
+            WHERE
+                Q1.FLAG = '0'
+                AND NOT EXISTS (
+                    SELECT 1
+                    FROM TB_USERQST Q2
+                    WHERE Q2.FLAG = '1'
+                      AND Q2.CHSEQ = Q1.QSTSEQ
+                )
+            ORDER BY
+                Q1.INDATEM ASC;
+            """;
+
+        List<Map<String,Object>> items = this.sqlRunner.getRows(sql, null);
+
+        return items;
+    }
+    // 차트데이터(일주일 가입자현황)
+    public List<Map<String, Object>> SignupOfWeek() {
+
+        String sql = """
+                SELECT
+                    CAST(INDATEM AS DATE) AS DATE,
+                    DATENAME(WEEKDAY, INDATEM) AS DAY,
+                    COUNT(*) AS CNT
+                FROM
+                    MOB_FACTCHK.dbo.TB_USERINFO
+                WHERE
+                    INDATEM >= CAST(GETDATE() - 6 AS DATE)
+                    AND INDATEM < CAST(GETDATE() + 1 AS DATE)
+                GROUP BY
+                    CAST(INDATEM AS DATE),
+                    DATENAME(WEEKDAY, INDATEM)
+                ORDER BY
+                    DATE;
+            """;
+//        WITH DateRange AS (
+//                SELECT CAST(GETDATE() - 6 AS DATE) + ROW_NUMBER() OVER (ORDER BY (SELECT NULL)) - 1 AS INDATEM
+//                FROM master..spt_values
+//        WHERE type = 'P' AND number < 7
+//              )
+//        SELECT
+//        DR.INDATEM AS DATE,
+//                DATENAME(WEEKDAY, DR.INDATEM) AS DAY,
+//        COUNT(UI.INDATEM) AS CNT
+//        FROM
+//        DateRange DR
+//        LEFT JOIN
+//        MOB_FACTCHK.dbo.TB_USERINFO UI ON CAST(UI.INDATEM AS DATE) = DR.INDATEM
+//        GROUP BY
+//        DR.INDATEM,
+//                DATENAME(WEEKDAY, DR.INDATEM)
+//        ORDER BY
+//        DR.INDATEM;
+
+
+        List<Map<String,Object>> items = this.sqlRunner.getRows(sql, null);
+
+        return items;
+    }
+    // 차트데이터(유출자현황)
+    public List<Map<String, Object>> Signout() {
+
+        String sql = """
+                
+            """;
+
+        List<Map<String,Object>> items = this.sqlRunner.getRows(sql, null);
+
+        return items;
+    }
+    // 차트데이터(등기열람건수)
+    public List<Map<String, Object>> CntOfRegister() {
+
+        String sql = """
+                
+            """;
+
+        List<Map<String,Object>> items = this.sqlRunner.getRows(sql, null);
+
+        return items;
+    }
+    // 차트데이터(상위검색지역)
+    public List<Map<String, Object>> BestSearch() {
+
+        String sql = """
+                
+            """;
+
+        List<Map<String,Object>> items = this.sqlRunner.getRows(sql, null);
+
+        return items;
+    }
+    // 카드 데이터(집계)
+    public List<Map<String, Object>> cardDatas() {
+
+        String sql = """
+            SELECT
+                SUM(CASE WHEN CONVERT(DATE, INDATEM) = CONVERT(DATE, GETDATE()) THEN 1 ELSE 0 END) AS TODAY_COUNT,
+                SUM(CASE WHEN CONVERT(DATE, INDATEM) = CONVERT(DATE, DATEADD(DAY, -1, GETDATE())) THEN 1 ELSE 0 END) AS YESTERDAY_COUNT
+            FROM
+                TB_USERINFO;
+            """;
+
+        List<Map<String,Object>> items = this.sqlRunner.getRows(sql, null);
+
+        return items;
+    }
+}
